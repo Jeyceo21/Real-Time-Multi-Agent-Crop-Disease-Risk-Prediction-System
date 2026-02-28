a5d08ecf805eb067192007b50d6b1972fe0d7f3a
Jeyceo21 <jeyanthanpetchimuthu@gmail.com>
2026-02-28 19:27:50 +0530

    Delete yield_model.py

--- yield_model.py
+++
@@ -1,7 +0,0 @@
-def regional_risk(lat: float, lon: float) -> float:
-    """
-    Temporary simplified regional risk.
-    Since we are not using database yet,
-    return moderate default risk.
-    """
-    return 0.3
